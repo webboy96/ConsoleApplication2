dc2917724b1804c1010b878889002fd0a4001b94
webboy96 <h8Pz8Wm@0dTE!1Eo!>
2023-10-21 16:37:24 +0500

    Add main and header

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+
+}
+
